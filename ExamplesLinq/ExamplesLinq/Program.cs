eede6404da36e8ed8712d8da394e088e949a441c
Dima <dmitiyoleynik@gmail.com>
2020-01-10 10:44:23 +0200

    Added User class

--- ExamplesLinq/ExamplesLinq/Program.cs
+++ ExamplesLinq/ExamplesLinq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ExamplesLinq
 {
@@ -6,7 +7,7 @@ namespace ExamplesLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
+
     }
 }
---
+++ ExamplesLinq/ExamplesLinq/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamplesLinq
+{
+    class User
+    {
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+
+        public string Hobby { get; set; }
+
+        public string Language { get; set; }
+
+    }
+}
